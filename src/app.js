35a5b1cb00edf820705ceec6c71e0a2956d45898
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2017-11-17 14:24:13 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static('public'));
+
+app.get('/todos', function(req, res) {
+  res.send('These are the todos!');
+});
+
+app.listen(3000, function() {
+  console.log("The server is running on port 3000!");
+});
